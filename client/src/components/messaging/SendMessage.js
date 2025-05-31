8ea524f3aa8fc7decf1084506de57a3e069eb456
nevine1 <venasamaan@gmail.com>
2025-05-30 23:00:37 -0400

    working on uplaod image and video message

--- client/src/components/messaging/SendMessage.js
+++ client/src/components/messaging/SendMessage.js
@@ -1,55 +1,124 @@
 "use client";
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { FaPlus } from "react-icons/fa";
 import { AiFillPicture } from "react-icons/ai";
 import { IoSend } from "react-icons/io5";
 import { FaVideo } from "react-icons/fa";
 import Image from 'next/image'
+import { IoMdCloseCircle } from "react-icons/io";
+
 const SendMessage = () => {
     const [click , setClick ] = useState(true)
     const [showMediaOptions, setShowMediaOptions] = useState(false);
-   const [uploadImage, setUploadImage ] = useState("")
+    
     const [message, setMessage ] = useState({
-        text: "", 
-        imageUrl: "", 
-        videoUrl:""
-    })
-    const handleUploadImage = (e) => {
-        const file = e.target.files?.[0] || null; 
-        setUploadImage(file)
-        setMessage((prev) => ({
-          ...prev,
-          imageUrL: file || "",
-        }));
+      text: "", 
+      imageUrl: "", 
+      videoUrl:""
+  })
+
+  const [file, setFile] = useState(null);
+  const [videoFile, setVideoFile ] = useState(null)
+  const [previewUrl, setPreviewUrl] = useState("");
+  const [previewVideoUrl, setPreviewVideoUrl] = useState(null)
+  
+  useEffect(() => {
+    if (!file) {
+      setPreviewUrl('');
+      return;
     }
-    const handleUploadVideo = (e) => {
-      const file = e.target.files?.[0] || null; 
-      setUploadImage(file)
-      setMessage((prev) => ({
-        ...prev,
-        videoUrl: file || "",
-      }));
-  }
-  console.log(message.imageUrl)
+  
+    const url = URL.createObjectURL(file);
+    setPreviewUrl(url);
+    
+    setMessage((prev) => ({ ...prev, imageUrl: url }));
+    
+    return () => URL.revokeObjectURL(url);
+  }, [file]);
+  
+  //  Handle image file selection
+  const handleUploadImage = (e) => {
+    const selectedFile = e.target.files?.[0] || null;
+    setFile(selectedFile); 
+  };
+  
+  // Clear uploaded image
+  const clearUploadImage = () => {
+    setFile(null); 
+    setMessage((prev) => ({ ...prev, imageUrl: '' }));
+  };
+
+  useEffect(() => {
+    if (!videoFile) {
+      setPreviewVideoUrl('');
+      return;
+    }
+  
+    const url = URL.createObjectURL(videoFile);
+    setPreviewVideoUrl(url);
+  
+  
+    setMessage((prev) => ({ ...prev, videoUrlUrl: url }));
+  
+ 
+    return () => URL.revokeObjectURL(url);
+  }, [videoFile]);
+ 
+  const handleUploadVideo = (e) => {
+    const selectedVideoFile = e.target.files?.[0] || null;
+    setVideoFile(selectedVideoFile); 
+  };
+  
+ 
+  const clearUploadVideo = () => {
+    setVideoFile(null); 
+    setMessage((prev) => ({ ...prev, videoUrlUrl: '' }));
+  };
+   
+
   return (
     
     <div >
       {/* show all messages  */}
-       <section className=" my-3 mx-2 p-3 bg-slate-300 h-[calc(100vh-17rem)] overflow-x-hidden overflow-y-scroll">
-        {
-          message.imageUrl && (
-            <div className="flex flex-col justify-center items-center bg-pink-50">
-          <Image
-            src={message.imageUrl}
-            width={150}
-                height={150}
-                alt="send image"
-            className="rounded-md bg-white w-40 h-40  m-4 p4"
-          />
-        </div>
-          )
-        }
+      <section className=" my-3 mx-2 p-3 bg-slate-300 h-[calc(100vh-17rem)] overflow-x-hidden overflow-y-scroll">
+        
+        {/* { upload image file } */}
+       {previewUrl && (
+          <div className="flex flex-col justify-center gap-3 items-center bg-pink-50 relative">
+            <div className="absolute top-1 cursor-pointer text-blue-500">
+              <IoMdCloseCircle size={22}
+                onClick={clearUploadImage}
+              />
+            </div>
+            <Image
+              src={previewUrl}
+              width={150}
+              height={150}
+              alt="send image"
+              className="rounded-md bg-white w-40 h-40 m-4 p-4"
+            />
+          </div>
+        )}
           
+        
+        {/* upload video file */}
+
+        {previewVideoUrl && (
+          <div className="flex flex-col justify-center gap-3 items-center bg-pink-50 relative">
+            <div className="absolute top-1 cursor-pointer text-blue-500">
+              <IoMdCloseCircle size={22}
+                onClick={clearUploadImage}
+              />
+            </div>
+            <video
+              src={previewVideoUrl}
+              width={150}
+              height={150}
+           
+              className="rounded-md bg-white w-40 h-40 m-4 p-4"
+            />
+          </div>
+        )}
        <h1>Show messsages</h1>
      
       </section>
@@ -87,14 +156,18 @@ const SendMessage = () => {
             <label htmlFor="uploadVideo" className="flex items-center gap-2  rounded-sm cursor-pointer p-2 pl-3  transition-all duration-200 hover:bg-white ">
               <FaVideo className="bg-blue-500 text-white rounded-full p-2 w-7 h-7  border hover:border-blue-500 " />
               <p className="text-[14px]">Video</p>
-            </label>
+                </label>
+                
              <input type="file"
-                   id="uploadImage"   
-                 onChange={handleUploadImage}         
-                      />
+                 id="uploadImage"   
+                  onChange={handleUploadImage} 
+                 className="hidden" 
+                />
+                
              <input type="file"
                 id="uploadVideo"   
-                 onChange={handleUploadVideo}         
+                  onChange={handleUploadVideo} 
+                 className="hidden" 
                 />         
           </div>
         )}
